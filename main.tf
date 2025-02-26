2a0604ef2372d720c0197a8391a635f13d888e26
Shankarsharm <80143973+Shankarsharm@users.noreply.github.com>
2025-02-26 19:41:33 +0530

    Reverted back original module code

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
-terraform {
-    backend "s3" {
-        bucket = "shanakr-tfbucket"
-        region = "ap-south-1"
-        key = "terraform-state"
-    }
+provider "aws" {
+    region = "ap-south-1"
+}
+
+module "ec2-instance" {
+    source = "./modules/test"
+    instance_type = "t2.micro"
+    ami_id = "ami-0d682f26195e9ec0f"
+    volume_size = 15
+}
+
+output "public_ip" {
+    value = module.ec2-instance.print_public_ip
+}
+
+output "pub_ip_pvt_machine" {
+    value = module.ec2-instance.print_pvt_ip
 }
